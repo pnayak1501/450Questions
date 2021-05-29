5676808f381c2f4a828a27e2ea28e60a35b2ee41
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-29 10:44:14 +0530

    Create 01 level order traversal.cpp

---
+++ BinaryTrees/01 level order traversal.cpp
@@ -0,0 +1,25 @@
+// https://practice.geeksforgeeks.org/problems/level-order-traversal/1
+
+vector<int> levelOrder(Node* node)
+    {
+      //Your code here
+      queue<Node*> q;
+      vector<int> ans;
+      q.push(node);
+      q.push(NULL);
+      Node* c;
+      while(!q.empty()){
+          c = q.front();
+          q.pop();
+          if(c!=NULL){
+            ans.push_back(c->data);
+            if(c->left != NULL)
+                q.push(c->left);
+            if(c->right != NULL)
+                q.push(c->right);
+          }else if(!q.empty()){
+              q.push(NULL);
+          }
+      }
+      return ans;
+    }
