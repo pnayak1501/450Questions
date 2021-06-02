7f1c39b53fe7a0ac32be811f9c4eaf8e8bb8c200
Prahlad Nayak <pnayak1501@gmail.com>
2021-06-02 17:33:34 +0530

    Create 214 FindavalueinaBST.cpp

---
+++ 214 FindavalueinaBST.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/search-in-a-binary-search-tree/submissions/
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL || root->val == val){
+            return root;
+        }
+        if(val<root->val){
+            return searchBST(root->left, val);
+        }
+            return searchBST(root->right, val);
+        
+    }
+};
