d7d04d8ee11962dbefe6e83fe1b2db9911b5437c
Prahlad Nayak <pnayak1501@gmail.com>
2021-06-02 17:27:26 +0530

    Update 178 ReverseLevelOrdertraversal.cpp

--- 178 ReverseLevelOrdertraversal.cpp
+++ 178 ReverseLevelOrdertraversal.cpp
@@ -1,5 +1,30 @@
 // https://practice.geeksforgeeks.org/problems/reverse-level-order-traversal/1#
 
+//METHOD 1
+vector<int> reverseLevelOrder(Node* root)
+{
+    stack <Node *> s;
+    queue <Node *> q;
+    q.push(root);
+    vector<int> ans;
+    while (!q.empty())
+    {
+        root = q.front();
+        q.pop();
+        ans.insert(ans.begin(), root->data);
+  
+        if (root->right)
+            q.push(root->right); 
+  
+        if (root->left)
+            q.push(root->left);
+    }
+
+    return ans;
+}
+
+
+//METHOD 2
 vector<int> reverseLevelOrder(Node* root)
 {
     stack <Node *> s;
